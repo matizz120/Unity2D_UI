f2ff6097e532010ebcef0e7594827a12892fb357
Matīss Petrausks <matiss44555@gmail.com>
2021-03-01 16:06:12 +0200

    1. Lekcijas uzdevums pabeigts

---
+++ Assets/Skripti/Vardaparadishana.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Jāieimportē, lai varētu strādāt ar UI elementiem
+using UnityEngine.UI;
+
+public class Vardaparadishana : MonoBehaviour{
+    //Mainīgajā saglabās tekstu, ko raksta teksta laukā
+    public string teksts;
+    //Lauks, kura ieraksta tekstu
+    public GameObject ievadeslauks1;
+    //attēlo tekstu
+    public GameObject tekstaAtteloshana;
+
+    //Funkcija tiek izsaukta nospiežot pogu
+    public void uzglabatTekstu(){
+        teksts = ievadeslauks1.GetComponent<Text>().text;
+        tekstaAtteloshana.GetComponent<Text>().text = "Sveiks, "+teksts.ToUpper() + "!";
+    }
+
+}
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Unity2D_UI
 
 ### Veicamo darbu saraksts
-- [ ] Nodrošināt projekta versionēšanu
-- [ ] Ievietot teksta logu
+- [x] Nodrošināt projekta versionēšanu
+- [x] Ievietot teksta logu
 - [ ] Ievietot attēlus
 - [ ] Ievietot ček boksus
 - [ ] Nodrošināt radio pogas darbību
